c19d544451872a5b461798e17cf2ae78dbc47f25
jingqi1990 <100445887@qq.com>
2017-05-04 16:17:16 +0800

    first2

---
+++ loadImage.js
@@ -0,0 +1,46 @@
+function preload(imgs){
+	this.imgs = imgs;
+	this.imgTotal  = imgs.length;
+	for (var i = 0; i < imgs.length; i++) {
+		this.imgs[i].img = new Image();
+	}
+	this.imgloaded = 0;
+	this.progress = 0;
+	self = this;
+}
+
+preload.prototype.load = function(callback){
+	for (var i = 0; i < this.imgTotal; i++) {
+		this.imgs[i].img.src = this.imgs[i].src;
+		this.imgs[i].img.id = this.imgs[i].id;
+		this.imgs[i].img.onload = function(){
+			if(this.complete){
+				self.imgloaded++;
+				self.progress = self.imgloaded / self.imgTotal;
+				self.getProgress(callback);
+			}					
+		}
+		this.imgs[i].img.onerror = function(){
+			self.getError(this.id);
+		}
+	}
+}
+
+preload.prototype.getProgress = function(theFunc){
+	if(theFunc){
+		theFunc(this.progress);
+	}
+}
+
+preload.prototype.getComplete = function(callback){
+	timer = setInterval(function(){
+		if(self.imgloaded == self.imgTotal){
+			callback();
+			clearInterval(timer);
+		}
+	},100);
+}
+
+preload.prototype.getError = function(errorImg){
+	console.log(errorImg+'有错误！');
+}
